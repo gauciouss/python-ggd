fc35bff3469776ec90902a5a2bfd476890f58d0c
何立偉 <>
2019-08-04 22:38:19 +0800

    新增取得年度休市日

--- ggd/util/ggdUtil.py
+++ ggd/util/ggdUtil.py
@@ -1,5 +1,9 @@
 import time
 import calendar
+import inspect
+import csv
+import requests
+from io import StringIO
 
 class Profiler:
 
@@ -12,6 +16,28 @@ class Profiler:
         now = time.time()
         t = now - self.start
         return t
+    
+    def startLog(self, msg = "", *p):
+        stack = inspect.stack()
+        the_class = stack[1][0].f_locals["self"].__class__
+        the_method = stack[1][0].f_code.co_name
+        logStr = "[START] {c}.{m}(). ".format(c = the_class, m = the_method)
+        logStr = logStr + msg
+        if len(p) > 0:
+            logStr = logStr.format(*p)
+        return logStr
+    
+    def endLog(self, msg = "", *p):
+        stack = inspect.stack()
+        the_class = stack[1][0].f_locals["self"].__class__
+        the_method = stack[1][0].f_code.co_name
+        logStr = "[END] {c}.{m}(), exec TIME: {t} ms. ".format(c = the_class, m = the_method, t = self.executeTime())
+        logStr = logStr + msg
+        if len(p) > 0:
+            logStr = logStr.format(*p)
+        return logStr
+        
+        
 
 
 class FinanceDateUtil:
@@ -22,3 +48,33 @@ class FinanceDateUtil:
     def isLastMonthDay(self, y, m, d):        
         ld = calendar.monthrange(y, m)[1]
         return True if d == ld else False
+
+
+    def GetHoliday(self, year):
+        url = 'https://www.twse.com.tw/holidaySchedule/holidaySchedule?response=csv&queryYear={d}'.format(d = year)
+        resp = requests.get(url)
+        t = resp.text
+        #sp = t.split("\n")
+        ##前3行去掉
+        f = StringIO(t)
+        reader = csv.reader(f, delimiter = ",")
+        rs = {}
+        i = 0
+        year = year + 1911
+        for r in reader:
+            if i < 2:
+                i = i + 1
+                continue
+            name = r[0].strip()
+            date = r[1]            
+
+            ds = date.split("日")
+            _d = []
+            for d in ds:              
+                if d == "":
+                    continue  
+                d = d.replace("月", "/")                
+                d = str(year) + "/" + d                
+                _d.append(d)
+            rs[name] = _d 
+        return rs
